6caa00b7332b3f206e5b1371e90a1f44c7ed38e4
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-06 08:58:30 +0300

    Create index.ts

---
+++ assets/index.ts
@@ -0,0 +1,10 @@
+const Images = {
+  cloth: require('./images/cloth.jpg'),
+  cafe: require('./images/cafe.jpg'),
+  coffee: require('./images/coffee.jpg'),
+  phone: require('./images/phone.jpg'),
+  // Add a fallback in case of missing images
+  placeholder: require('./images/placeholder.jpg')
+};
+
+export default Images;
