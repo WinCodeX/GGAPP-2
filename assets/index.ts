fae97b0cf19b4d15f34cbdc28a3fcecaeacc856e
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-06 17:28:17 +0300

    Update index.ts
    fixed

--- assets/index.ts
+++ assets/index.ts
@@ -4,6 +4,8 @@ const Images = {
   coffee: require('./images/coffee.jpg'),
   phone: require('./images/phone.jpg'),
   // Add a fallback in case of missing images
+  avatarPlaceholder: require('./images/avatar-placeholder.png'),
+  // Add other centralized images here
   
 };
 
